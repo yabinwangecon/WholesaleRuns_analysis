7a12c67f750b3219dc6028915052be2ee8da7a4e
Yabin W <yabin.wang.econ@gmail.com>
2022-09-02 13:45:18 +0200

    Update ThresholdSolver.R

--- ThresholdSolver.R
+++ ThresholdSolver.R
@@ -3,3 +3,17 @@
 
 # preliminaries -----------------------------------------------------------
 library(here)
+
+# set parameters ----------------------------------------------------------
+
+rho=0.049
+delta=1/0.101
+phi=1/5.8
+mu=0.049
+theta=0.449
+sigma=0.043
+rbar=0.00598
+alpha=0.92
+xstar_predicted=1/92
+l=alpha*phi/(rho+phi-mu)
+xstar_guess=
