50c95bfb935f46bc8cc0b78302649716d16e8eca
jacopomagnani <36220363+jacopomagnani@users.noreply.github.com>
2022-09-02 21:37:02 +0200

    Update ThresholdSolver.R

--- ThresholdSolver.R
+++ ThresholdSolver.R
@@ -14,20 +14,23 @@ mu=0.049
 theta=0.449
 sigma=0.043
 rbar=0.00598
+Rbar=(rbar+rho)/(phi+delta)+1
 alpha=0.92
 xstar_predicted=1/92
 l=alpha*phi/(rho+phi-mu)
+Wlimit=(phi+theta)/(phi+theta+rho)
+  
 
 # set parameters of the solution algorithm -----------------------
 
-xstar_guess=0.1
+xstar_guess=0.0108
 xbig=2
 
 
 # compute value function below threshold using analytical solution --------
 # note: using only expressions needed to evaluate function at threshold
 xstar=xstar_guess
-a1=mu+delta-delta*rbar
+a1=mu+delta-delta*Rbar
 a2=sigma^2/2
 a3=-(phi+rho+theta*delta+delta)
 a4=theta*delta*l+phi*(xstar<=1)
@@ -35,13 +38,13 @@ a5=delta+phi*(xstar>=1)
 eta=1/(2*a2)*(a2-a1+sqrt((a2-a1)^2-4*a3*a2))
 gamma=-1/(2*a2)*(a2-a1-sqrt((a2-a1)^2-4*a3*a2))
 if(xstar<=1){
-  A1=(1/rbar+a5/a3)*xstar^(-eta)+a4/(a3+a1)*xstar^(1-eta)
+  A1=(1/Rbar+a5/a3)*xstar^(-eta)+a4/(a3+a1)*xstar^(1-eta)
   FirstDerWxstar=A1*eta*xstar^(eta-1)-a4/(a3+a1)
   SecondDerWxstar=A1*eta*(eta-1)*xstar^(eta-2)
 }
 if(xstar>1){
   B2=phi/(gamma+eta)*((1-eta)/(a3+a1)+eta/a3)
-  A2=(1/rbar+a5/a3)*xstar^(-eta)+a4/(a3+a1)*xstar^(1-eta)-B2*xstar^(-gamma-eta)
+  A2=(1/Rbar+a5/a3)*xstar^(-eta)+a4/(a3+a1)*xstar^(1-eta)-B2*xstar^(-gamma-eta)
   B1=A2+phi/(gamma+eta)*(gamma/a3-(gamma+1)/(a3+a1))
   FirstDerWxstar=B1*eta*xstar^(eta-1)+B2*(-gamma)*xstar^(-gamma-1)-a4/(a3+a1)
   SecondDerWxstar=B1*eta*(eta-1)*xstar^(eta-2)+B2*(-gamma)*(-gamma-1)*xstar^(-gamma-2)
@@ -55,7 +58,7 @@ parameters <- c(mu=mu,
                 sigma=sigma,
                 phi=phi,
                 rho=rho)
-state<- c(W=1/rbar,
+state<- c(W=1/Rbar,
           Z=FirstDerWxstar)
 derivatives <- function(x,state,parameters){
   with(as.list(c(state,parameters)), {
@@ -66,6 +69,7 @@ derivatives <- function(x,state,parameters){
 }
 
 
-ode(y=state, times=c(xstar,xbig), func=derivatives, parms=parameters)
+results<-ode(y=state, times=c(xstar,xbig), func=derivatives, parms=parameters)
+Wbig<-results[2,2]
 
 #compare compute W at xbig with analytical limit
